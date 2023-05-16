6064942f878e8727cba67f99740f88e7291a8a24
Subhojeet Dutta <103040725+Subhojeet28@users.noreply.github.com>
2023-05-16 11:26:23 +0530

    Calculator

---
+++ Calculator.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    int num1,num2;
+    cout<<"Enter your number1:";
+    cin>>num1;
+    cout<<"Enter your number2:";
+    cin>>num2;
+
+    cout<<"Addition of the number is:"<<num1+num2<<endl;
+    cout<<"Multiplication of the number is:"<<num1*num2<<endl;
+    cout<<"Division of the number is:"<<num1/num2<<endl;
+    cout<<"Subtraction of this number is:"<<num1-num2<<endl;
+
+}
+
